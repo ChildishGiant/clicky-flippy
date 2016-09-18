e2dabce15cb3c5e8f46688d5ab603ae5ff8b0bbc
SolarPolarMan <SolarPolarMan@gmail.com>
2016-09-18 12:17:39 +0100

    reworked system

--- jssource.js
+++ jssource.js
@@ -7,26 +7,51 @@ $(function() {
 
 
 function randomiseTiles(){
-  var ids = [ 'orange', 'blue' ];
-
   $('.tile').each(function(i) {
-    $(this).attr("id", ids[Math.floor( Math.random()*ids.length)]);
+    //wipe classes
+    $(this).removeClass();
+    //add tile class again.
+    $(this).addClass("tile")
+    //add either orange or blue
+    if (Math.random() > 0.5){
+      $(this).addClass("blue");
+    }else{
+      $(this).addClass("orange");
+    };
   });
 
 };
+
+function toggleColour(element){
+
+  //If the tile has the class of blue
+  if ($(element).attr('class').indexOf("blue") !== -1){
+    $(element).addClass("orange").removeClass("blue");
+
+  }else{
+    $(element).addClass("blue").removeClass("orange");
+
+  };
+
+}
 
 function invert(clicked){
   $('.tile').each(function(i) {
 
     if (this != clicked){
-
-      if ($(this).attr("id") == "blue"){
-        $(this).attr("id", "orange");
-      }else{
-        $(this).attr("id", "blue");
-      };
+        toggleColour(this);
     };
   });
+};
+
+function invertRow(rowNumber){
+
+  $('.tile').each(function(i) {
+    if ($(this).attr("id")[0] != rowNumber){
+        toggleColour(this);
+    };
+  });
+
 };
 
 Number.prototype.times = function(fn) {
@@ -39,7 +64,7 @@ function makeGrid(numRows, numCols) {
 
     var row = function(r) {
         return $("<tr/>").append(numCols.times(function(c) {
-            return $("<td/>").html("<div class=\"tile\" onclick=\"invert(this);\"></div>");
+            return $("<td/>").html("<div class=\"tile\" id=\"" + r + c + "\" onclick=\"invert(this);\"></div>");
         }));
     };
 
